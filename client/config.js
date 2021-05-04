bbc13968571f2a330042727b9992074ef3610be4
lukas-jj <lukas.jury13@gmail.com>
2021-05-04 12:00:13 +1200

    deploy plz

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 const environments = {
-    development: 'http://localhost:3000/api/v1',
-    test: 'http://localhost:3333/api/v1',
-    production: 'https://acoustafind.herokuapp.com/api/v1',
+    development: 'http://localhost:3000/api',
+    test: 'http://localhost:3333/api',
+    production: 'https://acoustafind.herokuapp.com/api',
     integration: '',
     deployment: '',
     build: ''
--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 
-if(!process.env.NODE_ENV || process.env.NODE_ENV == 'development') {
-  const envConfig = require('dotenv').config()
-  if(envConfig.error) throw envConfig.error
-}
+
 
 const server = require('./server')
 
